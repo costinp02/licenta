a55fad88171d669ef4a0d16b7f581c4000826d52
costinp02 <80647850+costinp02@users.noreply.github.com>
2023-09-01 12:04:15 +0300

    more UI changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,8 @@
 import logo from '../assets/images/react-logo.png';
-import './Header.css';
 
 export default function Header(props) {
   return (
-    <nav className="nav-bar"  style={{backgroundColor: "#00e673", width:"100%"}}>
+    <nav className="nav-bar"  >
       <p>
         <img src={logo} alt="logo" height="40" />
       </p>
